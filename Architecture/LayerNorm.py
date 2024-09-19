19a1e58f865c80bd709a48422f45300c0594d455
Alo Emmanuel Temitope <emmanuelalo52@gmail.com>
2024-09-19 12:04:50 +0100

    Debugged layerNorm and changed the names of the parameters for multi-head attention.

--- Architecture/LayerNorm.py
+++ Architecture/LayerNorm.py
@@ -2,27 +2,15 @@ import torch
 import torch.nn as nn
 
 class LayerNorm(nn.Module):
-    def __init__(self, dim, eps=1e-5, momentum=0.1):
+    def __init__(self, dim, eps=1e-5):
         super().__init__()
         self.eps = eps
-        self.momentum = momentum
-        self.gamma = nn.Parameter(torch.ones(dim))
-        self.beta = nn.Parameter(torch.zeros(dim))
-        self.register_buffer('running_mean', torch.zeros(dim))
-        self.register_buffer('running_var', torch.ones(dim))
+        self.gamma = torch.ones(dim)
+        self.beta = torch.zeros(dim)
 
     def forward(self, x):
-        if self.training:
-            dim = tuple(range(x.ndim - 1))
-            mean = x.mean(dim, keepdim=True)
-            var = x.var(dim, unbiased=False, keepdim=True)
-            
-            with torch.no_grad():
-                self.running_mean = (1 - self.momentum) * self.running_mean + self.momentum * mean.squeeze()
-                self.running_var = (1 - self.momentum) * self.running_var + self.momentum * var.squeeze()
-        else:
-            mean = self.running_mean.unsqueeze(-1)
-            var = self.running_var.unsqueeze(-1)
-
+        mean = x.mean(1, keepdim=True)
+        var = x.var(1, keepdim=True)
         x_norm = (x - mean) / torch.sqrt(var + self.eps)
-        return self.gamma * x_norm + self.beta
+        self.out = self.gamma * x_norm + self.beta
+        return self.out
